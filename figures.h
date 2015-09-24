87f9123f7b3d5104c46b44a3eb2885030ae250cb
Adam Balogh <balogh.adam@icloud.com>
2015-09-24 20:06:23 +0100

    improve const correctness

--- figures.h
+++ figures.h
@@ -16,14 +16,14 @@ struct Figure {
   const FigureShape& shape;
   int orientation;
 
-  Figure(FigureType type, FigureShape& shape, int orientation)
+  Figure(FigureType type, const FigureShape& shape, int orientation)
       : type(type), shape(shape), orientation(orientation) {}
 
   int Height() const { return shape.size(); }
   int Width() const { return shape[0].size(); }
 };
 
-// Creates and Rotates Figures.
+// Creates and Rotates Figures in an efficient way.
 //
 // FigureManager must outlive Figures that were returned by it.
 class FigureManager {
@@ -32,19 +32,20 @@ class FigureManager {
 
   // Returns a figure with the given type and orientation.
   Figure MakeFigure(const FigureType type, const int orientation) {
-    return Figure{type, shapes_[orientation][type], orientation};
+    return Figure{type, shapes_.at(orientation).at(type), orientation};
   }
 
   // Returns a random figure with up orientation.
-  inline Figure GetRandomUpFigure() {
+  inline Figure GetRandomUpFigure() const {
     auto type = static_cast<FigureType>(std::rand() % NUM_TYPES);
-    return Figure{type, shapes_[0][type], 0};
+    return Figure{type, shapes_.at(0).at(type), 0};
   }
 
   // Rotates the given figure clockwise
-  Figure GetRotated(const Figure& f) {
+  Figure GetRotated(const Figure& f) const {
     int new_orientation = (f.orientation + 1) % 4;
-    return Figure{f.type, shapes_[new_orientation][f.type], new_orientation};
+    return Figure{f.type, shapes_.at(new_orientation).at(f.type),
+                  new_orientation};
   }
 
  private:
